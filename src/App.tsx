96618029d14854e2ef6afa32dbff18263a243619
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-03-23 14:01:58 -0300

    Aplicação da AppBar e da Div Body

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import AppBar from "./componentes/AppBar";
 
 function App() {
-  return <div className="bg-blue-500">Teste</div>;
+  return (
+    <>
+      <AppBar />
+      <div className="bg-slate-800 relative top-16 min-h-full">teste</div>
+    </>
+  );
 }
 
 export default App;
