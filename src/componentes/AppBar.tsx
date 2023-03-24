bd5c1ebd02e92db55a396169c0e491118af01230
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-03-24 14:05:25 -0300

    Alteracao da index.tsx e da AppBar.tsx

--- src/componentes/AppBar.tsx
+++ src/componentes/AppBar.tsx
@@ -1,17 +1,23 @@
+import { Button } from "./Button";
+
 export default function AppBar() {
   return (
-    <nav id="navBar" className="bg-gray-900 h-16 w-full flex fixed z-10">
-      <div className="h-full text-3xl font-serif flex gap-2 grow items-center">
-        <img className="w-16 px-2" src="img/notepad.png" alt="logo"></img>
-        <h1 className="text-white">
-          Projeto de NotePad Com ReactJS e TailwindCSS
-        </h1>
+    <header
+      id="navBar"
+      className="bg-gray-900 h-16 min-w-[380px] w-full flex items-center sticky top-0 left-0"
+    >
+      <div className="h-full text-2xl md:text-3xl lg:text-4xl font-serif flex gap-2 grow items-center">
+        <img className="w-14 px-2" src="img/notepad.png" alt="logo"></img>
+        <h1 className="text-white">Jon's Notes</h1>
       </div>
       <div className="flex items-center px-2">
-        <button className="bg-[#03ACB3] text-white text-2xl p-2 rounded-2xl">
+        <Button
+          onClick={() => {}}
+          className="bg-[#03ACB3] hover:bg-[#0fd8df] text-white text-lg btn-text-shadow p-1 rounded-2xl md:text-xl md:p-2 lg:text-2xl"
+        >
           Nova Notificação
-        </button>
+        </Button>
       </div>
-    </nav>
+    </header>
   );
 }
