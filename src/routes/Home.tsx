e20fb4075dd0a3a612cc64bfee78815bcf8c2b0f
Jonathan Silva <jonalves.silva@jsmail.com.br>
2023-03-29 15:17:28 -0300

    Alteração arquivo App.css e Home.tsx

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,39 @@
+import { useState, useEffect } from "react";
+import { api } from "../Api";
+import { NotepadList, Notepad } from "../componentes/NotepadList";
+
+async function pegaNotepads() {
+  const res = await api.get("/notepads");
+  const notepads = res.data;
+  notepads.reverse();
+  return notepads;
+}
+
+const initialNotepads: Notepad[] = [];
+
 export function Home() {
-  return <div className="">teste</div>;
+  const [notepads, setNotepads] = useState(initialNotepads);
+  const [isLoading, setIsLoading] = useState(false);
+  const textoCarregando = isLoading ? "Carregando..." : "";
+
+  useEffect(() => {
+    setIsLoading(true);
+    pegaNotepads().then((notepads) => {
+      setNotepads(notepads);
+      setIsLoading(false);
+    });
+  }, []);
+
+  useEffect(() => {
+    console.log(`isLoading é igual a: ${isLoading}`);
+  }, [isLoading]);
+
+  return (
+    <div>
+      <div className="w-full text-white flex justify-center md:text-xl">
+        {textoCarregando}
+      </div>
+      <NotepadList notepads={notepads} />
+    </div>
+  );
 }
